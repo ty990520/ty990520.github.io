aa036a6fc24e4e41cb7861a1486a6e064588af46
ty990520 <48792230+ty990520@users.noreply.github.com>
2019-05-26 19:58:59 +0900

    first commit

--- me/views.py
+++ me/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request,'home.html')
+    return render(request,'index.html')
 
 def profile(request):
     return render(request,'profile.html')   
